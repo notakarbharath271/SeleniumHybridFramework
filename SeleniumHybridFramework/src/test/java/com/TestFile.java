99913b839abb364d9e93215788ff896c2234ded0
Bharath <Bharath@DESKTOP-HMBR4KM>
2019-09-13 19:39:40 +0530

    Changes are done in the TestFile.java file

--- SeleniumHybridFramework/src/test/java/com/TestFile.java
+++ SeleniumHybridFramework/src/test/java/com/TestFile.java
@@ -1,1 +1,8 @@
 //This the file i created in the git Repository
+//This the file i created in the git Repository//This the file i created in the git Repository
+//This the file i created in the git Repository
+//This the file i created in the git Repository
+//This the file i created in the git Repository
+//This the file i created in the git Repository
+//This the file i created in the git Repository
+
